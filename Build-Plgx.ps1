6ea49fd2954e55cdb7ba81e62b40e17ebf854006
K.S.H <lacroixx13@gmail.com>
2022-06-20 20:10:26 +0200

    Fix variable refactoring and unused leftovers
    CTRL+R CTRL+R? Nah.

--- Build-Plgx.ps1
+++ Build-Plgx.ps1
@@ -7,17 +7,15 @@ KP2chan PLGX Build Script
 
 $pluginDir = Resolve-Path .\KP2chan
 
-$keepassDebugExecutable
-$keepassReleaseExecutable
 $plgxCompiler
 if (Test-Path .\out\Debug\KeePass.exe) {
     $plgxCompiler = Get-ChildItem .\out\Debug\KeePass.exe
 } elseif (Test-Path .\out\Release\KeePass.exe) {
-    $keepassExecutable = Get-ChildItem .\out\Release\KeePass.exe
+    $plgxCompiler = Get-ChildItem .\out\Release\KeePass.exe
 } else {
     Write-Warning "Can't build PLGX because the output KeePass was not found!"
 
-    $keepassExecutable = Read-Host -Prompt "Please specify a path to a KeePass executable" |
+    $plgxCompiler = Read-Host -Prompt "Please specify a path to a KeePass executable" |
     Resolve-Path
 }
 
@@ -25,9 +23,9 @@ Write-Output "Compiling..."
 
 # The safe way. No arguments redirects.
 if ($configuration -eq "Debug") {
-    Start-Process $keepassExecutable -ArgumentList "--plgx-create $pluginDir --debug"
+    Start-Process $plgxCompiler -ArgumentList "--plgx-create $pluginDir --debug"
 } else {
-    Start-Process $keepassExecutable -ArgumentList "--plgx-create $pluginDir"
+    Start-Process $plgxCompiler -ArgumentList "--plgx-create $pluginDir"
 }
 
 Write-Output "Finished. Until next time!"
